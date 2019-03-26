7b8a94408607c0d8f5b8969318a1582ad3f5c2f6
Liu Ying <victor.liu@nxp.com>
2019-03-26 13:36:52 +0800

    MLK-20208-1 mxc IPUv3: device: Remove a piece of dead code in ipu_task_thread()
    The check on !sp_tsk0 is unnecessary in ipu_task_thread(), because the
    beforehand "list_del(&sp_tsk0->node);" within the context implies sp_tsk0
    is not null, otherwise, we'll dereference a null pointer earlier.
    
    Detected by CoverityScan, CID#17842 ("Logically dead code")
    
    Signed-off-by: Liu Ying <victor.liu@nxp.com>
    (cherry picked from commit 9ad5edd076d61bc8bb3a558e523cc7b31f2c3043)

--- drivers/mxc/ipu3/ipu_device.c
+++ drivers/mxc/ipu3/ipu_device.c
@@ -3309,10 +3309,6 @@ static int ipu_task_thread(void *argv)
 				/* let the parent thread do the first sp_task */
 				/* FIXME: ensure the correct sequence for split
 					4size: 5/6->9/a*/
-				if (!sp_tsk0)
-					dev_err(tsk->dev,
-					"ERR: no-0x%x,can not get split_tsk0\n",
-					tsk->task_no);
 				wake_up_interruptible(&thread_waitq);
 				get_res_do_task(sp_tsk0);
 				dev_dbg(sp_tsk0->dev,
